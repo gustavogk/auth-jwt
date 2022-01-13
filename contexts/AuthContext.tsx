a0c52cf9714c9fde3bd58e5df074bc540ccc0482
Gustavo Assuncao <gust.krv@gmail.com>
2022-01-13 16:17:09 -0300

    cookies

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,13 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
+import { setCookie } from 'nookies'
+import Router from 'next/router';
+import { api } from "../services/api";
+
+type User = {
+email: string;
+permissions: string[];
+roles: string[];
+};
 
 type SignInCredentials = {
     email: string;
@@ -7,6 +16,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
     signIn(credentials: SignInCredentials): Promise<void>;
+    user: User;
     isAuthenticated: boolean;
 };
 
@@ -18,14 +28,43 @@ export const AuthContext = createContext({} as AuthContextData)
 
 export function AuthProvider({ children }: AuthProviderProps){
 
-    const isAuthenticated = false;
+    const [user, setUser] = useState<User>();
+    const isAuthenticated = !! user;
 
     async function signIn({ email, password }: SignInCredentials){
-        console.log({email,password})
+        try{
+            const response = await api.post('sessions', {
+                email,
+                password,
+            })
+
+            const { token, refreshToken, permissions , roles } = response.data;
+
+            setCookie(undefined, 'nextauth.token', token, {
+                maxAge: 60 * 60 * 24 * 30, //30 dias
+                path: '/'
+            })
+            
+            setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+                maxAge: 60 * 60 * 24 * 30, //30 dias
+                path: '/'
+            })
+
+            setUser({
+                email,
+                permissions,
+                roles,
+            })
+
+            Router.push('/dashboard');
+
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     return(
-        <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+        <AuthContext.Provider value={{ signIn, user ,isAuthenticated}}>
             {children}
         </AuthContext.Provider>
 
