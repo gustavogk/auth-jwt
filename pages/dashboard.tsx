890078a97020f95915a5f3b6bf7ace7d17887095
Gustavo Assuncao <gust.krv@gmail.com>
2022-01-16 22:09:22 -0300

    add permission control

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,14 +1,13 @@
 import { useContext, useEffect } from "react"
 import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext"
-import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from '../services/apiClient';
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard(){
 
-    const { user } = useContext(AuthContext)
+    const { user, signOut } = useContext(AuthContext)
 
     useEffect(() => {
         api.get('/me')
@@ -18,6 +17,9 @@ export default function Dashboard(){
     return(
         <>
             <h1>Dashboard: {user?.email}</h1>
+
+            <button onClick={signOut}> Sign out </button>
+            
             <Can permissions={[ "metrics.list" ]}>
                 <div>Metricas</div>
             </Can>
